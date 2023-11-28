f052e268240f197f2147d282c51909aefbaa3a26
Ivan Aguilar <ivanaguiglez@gmail.com>
2023-11-28 15:46:30 +0100

    Start server & connect to database

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-// Director de orquesta 🦥
+import "dotenv/config";
+import debugCreator from "debug";
+import { startServer } from "./server/app.js";
+import "./server/index.js";
+import chalk from "chalk";
+import { connectToDatabase } from "./database/index.js";
+
+const debug = debugCreator("meetMauritius:main");
+
+const port = process.env.PORT ?? 4000;
+
+if (!process.env.MONGODB_URL) {
+  debug(chalk.red("Missing MeetMauritius DB connection string"));
+  process.exit();
+}
+
+const mongoUrl = process.env.MONGODB_URL;
+
+await connectToDatabase(mongoUrl);
+debug(chalk.red("Connected to MeetMauritius DB"));
+
+startServer(+port);
