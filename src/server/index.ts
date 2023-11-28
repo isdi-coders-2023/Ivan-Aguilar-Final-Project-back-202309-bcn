9bc22c9e89a26cf6b149b82a3c51ac1c2cd19cc5
Ivan Aguilar <ivanaguiglez@gmail.com>
2023-11-28 16:43:53 +0100

    Add CORS origins

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,9 @@
-// La cadena de middlewares 📎
+import morgan from "morgan";
+import Express from "express";
+import cors from "cors";
+import app from "./app";
+
+const corsPort = process.env.ALLOWED_ORIGIN_URL;
+const corsOptions = { origin: corsPort };
+
+app.use(cors(corsOptions));
