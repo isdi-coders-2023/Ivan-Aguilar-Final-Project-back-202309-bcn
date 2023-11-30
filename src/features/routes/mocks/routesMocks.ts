2fe27dcdc458dc9022afbf80a23d7145b4639031
Ivan Aguilar <ivanaguiglez@gmail.com>
2023-11-30 11:23:50 +0100

    Add getRoutes test

---
+++ src/features/routes/mocks/routesMocks.ts
@@ -0,0 +1,37 @@
+const routesMock = [
+  {
+    _id: "6564a20f803b820996b53g44",
+    image:
+      "https://miro.medium.com/v2/resize:fit:1100/format:webp/0*IyAju_tGqm7eE7y5.jpg",
+    name: "Le Morne Brabant",
+    duration: 5,
+    zone: "West",
+    price: 70,
+    numberOfStops: 2,
+    description: "Aroute throgh the most important mountain of the island",
+  },
+  {
+    _id: "8564a20f803b820996b507fd",
+    image:
+      "https://media.tacdn.com/media/attractions-splice-spp-674x446/09/29/aa/ed.jpg",
+    name: "Seven Colored Earth Geopark",
+    duration: 5,
+    zone: "East",
+    price: 60,
+    numberOfStops: 1,
+    description: "Enjoy an unic place in the world that...",
+  },
+  {
+    _id: "6564a20f803b820996b508fg",
+    image:
+      "https://i.pinimg.com/originals/52/0a/ac/520aace25e0e8ea1eca48ae387718314.jpg",
+    name: "Île aux Cerfs",
+    duration: 5,
+    zone: "South-West",
+    price: 80,
+    numberOfStops: 3,
+    description: "Enjoy the paradise in this magic island...",
+  },
+];
+
+export default routesMock;
